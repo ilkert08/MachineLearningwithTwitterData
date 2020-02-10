c6c0eaa643f9b67b97c0179a169455a365596e6f
ilkert08 <42252372+ilkert08@users.noreply.github.com>
2020-02-10 16:59:50 +0300

    Clearing
    File has been cleared.

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,4 @@
 import tokenize
-X = [[3, 6], [4,8],[0,0], [6,10], [8,12]]
-y = [4, 4, 4, 7, 7]
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing
 from sklearn.neighbors import NearestNeighbors
@@ -21,7 +19,7 @@ def stopWordsRemoval():
 
     print("Start")
     stop_words = set(stopwords.words('english')) 
-    file1 = open("C:/Users/ilkertinkir/Desktop/FilteredText/nonfiltered55.txt", "r") 
+    file1 = open("C:/Users/user0/Desktop/FilteredText/nonfiltered55.txt", "r") 
     t = 0
     for  t in range(200000): 
         line = file1.readline() 
@@ -41,7 +39,7 @@ def wordSteaming():
     from nltk.stem import PorterStemmer 
     from nltk.tokenize import word_tokenize 
     print("giris!")
-    file1 = open("C:/Users/ilkertinkir/Desktop/FilteredText/lastfilter.txt")
+    file1 = open("C:/Users/user0/Desktop/FilteredText/lastfilter.txt")
     ps = PorterStemmer() 
     t = 0
 
@@ -53,7 +51,7 @@ def wordSteaming():
         for r in words5:
             words = word_tokenize(r) 
             for w in words: 
-                featureFile = open("C:/Users/ilkertinkir/Desktop/FilteredText/features.txt","a")
+                featureFile = open("C:/Users/user0/Desktop/FilteredText/features.txt","a")
                 featureFile.write(ps.stem(w) + " ")
                 #print(w, " : ", ps.stem(w))
         #print("ALT SATIR!")
@@ -61,7 +59,7 @@ def wordSteaming():
         #featureFile.close()
     
 def uniqueWords():
-    file1 = open("C:/Users/ilkertinkir/Desktop/FilteredText/sonhal/highFreqList.txt")
+    file1 = open("C:/Users/user0/Desktop/FilteredText/sonhal/highFreqList.txt")
     reader = file1.read()
     splt0 = reader.split()
     print(len(reader))
@@ -74,7 +72,7 @@ def uniqueWords():
     print(len(myList))
     #60902 All Unique Word Number
     #3951 High Freq > 10 
-    file1 = open("C:/Users/ilkertinkir/Desktop/FilteredText/sonhal/highFreqList.txt")
+    file1 = open("C:/Users/user0/Desktop/FilteredText/sonhal/highFreqList.txt")
     i = 0
     k = 0
     listofSet = []
@@ -88,7 +86,7 @@ def uniqueWords():
 #                if w == w2:
 #                    c = c + 1
 #            if c > 10:
-#                file2 = open("C:/Users/ilkertinkir/Desktop/FilteredText/highFreq.txt","a")
+#                file2 = open("C:/Users/user0/Desktop/FilteredText/highFreq.txt","a")
 #                print(w+":"+str(c))
 #                low = low + 1
 #                print("High:"+ str(low))
@@ -105,7 +103,7 @@ def uniqueWords():
         line = file1.readline()
         splitted2 = line.split()
         ss = ""
-        file2 = open("C:/Users/ilkertinkir/Desktop/FilteredText/sonhal/sonsayilar.txt","a")    
+        file2 = open("C:/Users/user0/Desktop/FilteredText/sonhal/sonsayilar.txt","a")    
         for word in splitted2:      
             ss = ss + " "+ str(myList.index(word))            
                 
@@ -118,8 +116,8 @@ def uniqueWords():
 
 
 def highFreqList():
-    file1 = open('C:/Users/ilkertinkir/Desktop/FilteredText/otuzbinsozcuk.txt', 'rt')
-    file2 = open('C:/Users/ilkertinkir/Desktop/FilteredText/highFreq.txt', 'rt')
+    file1 = open('C:/Users/user0/Desktop/FilteredText/otuzbinsozcuk.txt', 'rt')
+    file2 = open('C:/Users/user0/Desktop/FilteredText/highFreq.txt', 'rt')
     
     reader2 = file2.read()
     splt2 = reader2.split()
@@ -132,7 +130,7 @@ def highFreqList():
             for w2 in splt2:
                 if w == w2:
                     ss = ss + " " + w
-        file3 = open('C:/Users/ilkertinkir/Desktop/FilteredText/sonhal/highFreqList.txt', 'a')
+        file3 = open('C:/Users/user0/Desktop/FilteredText/sonhal/highFreqList.txt', 'a')
         file3.write(ss)
         file3.write("\n")
         file3.close()
@@ -148,7 +146,7 @@ def newList():
 def dataStabilizer():
    
      # 3951 Adet Unique Low Freq Sozcuk bulunmaktadir. Hesaplandi.
-    file_input = open('C:/Users/ilkertinkir/Desktop/FilteredText/sonhal/sonsayilar.txt', 'rt')
+    file_input = open('C:/Users/user0/Desktop/FilteredText/sonhal/sonsayilar.txt', 'rt')
 
 
     #CSV DOSYASININ 1,2,4 ve 5. sutunları kullanılacaktir(ID-1, DATE-2, NICKNAME-4, TWEET-5)
@@ -206,7 +204,7 @@ def dataStabilizer():
 def kNN ():
     dataStabilizer()
     print("Gecti!")
-    file_input = open('C:/Users/ilkertinkir/Desktop/FilteredText/sonhal/sonsayilar.txt', 'rt')
+    file_input = open('C:/Users/user0/Desktop/FilteredText/sonhal/sonsayilar.txt', 'rt')
     neigh = KNeighborsClassifier(n_neighbors=2)
     print("Training!")
     neigh.fit(convertedDataArray, labelArray)
@@ -270,7 +268,7 @@ def kNN ():
     print("Accuracy:", 100*(200-acc) / 200 )
 
 
-    text_file = open("C:/Users/ilkertinkir/Documents/NetBeansProjects/bilgiyeErisim1/info.txt", "a+")
+    text_file = open("C:/Users/user0/Documents/NetBeansProjects/bilgiyeErisim1/info.txt", "a+")
     nn = text_file.write("Test Datasi Sayisi: "+ str(200) + "\nDogru Tahmin Sayisi:"+ str(200 - acc) + "\nYanlis Tahmin Sayisi:"+str(acc)+ "\nDogru Tahmin Orani:"+ str(100*(200-acc) / 200) )
     text_file.close()
     file_input.close()
@@ -280,11 +278,9 @@ def kNN ():
 
 def decisionTree():
     from sklearn import tree
-    #X = [[0, 0], [1, 1]]
-    #Y = [0, 1]
     dataStabilizer()
     print("Gecti!")
-    file_input = open('C:/Users/ilkertinkir/Desktop/train_edit33.csv', 'rt')
+    file_input = open('C:/Users/user0/Desktop/train_edit33.csv', 'rt')
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(convertedDataArray, labelArray)
     i = 0
@@ -305,13 +301,7 @@ def decisionTree():
 
             
             
-  #          print("Tahmin:",clf.predict([[
-  #                        le.transform([  row[1], row[2], row[4], row[5]  ])[0],
-  #                        le.transform([  row[1], row[2], row[4], row[5]  ])[1],
-  #                        le.transform([  row[1], row[2], row[4], row[5]  ])[2],
-  #                        le.transform([  row[1], row[2], row[4], row[5]  ])[3] ]]))
-  #          print("Asil Sonuc:", row[0])
-        
+ 
         if i == 99999:
             print("Ara!")
         if i>100000 and i<105000:
@@ -329,19 +319,12 @@ def decisionTree():
 
            
 
-
-     #       print("Tahmin:",clf.predict([[
-     #                     le.transform([  row[1], row[2], row[4], row[5]  ])[0],
-     #                     le.transform([  row[1], row[2], row[4], row[5]  ])[1],
-     #                     le.transform([  row[1], row[2], row[4], row[5]  ])[2],
-     #                     le.transform([  row[1], row[2], row[4], row[5]  ])[3] ]]))
-     #       print("Asil Sonuc:", row[0])
         
         i = i + 1
 
     print("Accuracy:", 100 * (10000-acc)/10000)
     number = 100 * (1000-acc)/1000
-    text_file = open("C:/Users/ilkertinkir/Documents/NetBeansProjects/bilgiyeErisim1/info.txt", "a+")
+    text_file = open("C:/Users/user0/Documents/NetBeansProjects/bilgiyeErisim1/info.txt", "a+")
     nn = text_file.write("Test Datasi Sayisi: "+ str(10000) + "\nDogru Tahmin Sayisi:"+ str(10000 - acc) + "\nYanlis Tahmin Sayisi:"+str(acc)+ "\nDogru Tahmin Orani:"+ str(100*(10000-acc) / 10000) )    
     file_input.close()
     dataArray.clear()
@@ -355,7 +338,7 @@ def decisionTree():
 #uniqueWords()
     #le.fit(["i am so happy today", "i am so sad today", "i am so happy and funny"])
     #print(le.transform(["i am so happy today", "i am so sad today", "i am so happy and funny"]))
-file00 = open('C:/Users/ilkertinkir/Documents/NetBeansProjects/bilgiyeErisim1/pythonVariables.txt', 'rt')
+file00 = open('C:/Users/user0/Documents/NetBeansProjects/bilgiyeErisim1/pythonVariables.txt', 'rt')
 ss = file00.readline()
 if ss == "firstAlgorithm":
     print("Decision Tree:")
